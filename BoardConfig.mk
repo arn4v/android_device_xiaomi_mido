1d614b6e74f8a3b40b346f88cf2d1e3ae3d726c0
TheScarastic <warabhishek@gmail.com>
2017-05-23 15:06:02 +0000

    mido: Add cmhw services

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,7 +108,10 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
 # CMHW
 BOARD_USES_CYANOGEN_HARDWARE := true
-BOARD_HARDWARE_CLASS += hardware/cyanogen/cmhw
+BOARD_HARDWARE_CLASS += \
+    hardware/cyanogen/cmhw \
+    $(LOCAL_PATH)/cmhw
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
 
 # CNE / DPM
 BOARD_USES_QCNE := true

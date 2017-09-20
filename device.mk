a34d147228f4c86d4745a34eb7e4d342756d80dc
Arnav Gosain <arn4v@outlook.com>
2017-09-20 11:21:04 +0530

    Revert "mido: Move to oss consumerir hal"
    This reverts commit f7856aeb502bf00c57e4127ea4fe2e260a9dddf3.

--- device.mk
+++ device.mk
@@ -105,10 +105,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Snap
 
-# Consumerir
-PRODUCT_PACKAGES += \
-    consumerir.msm8953
-
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8953 \
